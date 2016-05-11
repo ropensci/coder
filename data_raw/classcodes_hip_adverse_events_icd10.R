7fa540e1f301672be73f3a99828ce5f147ac996c
Erik Bülow <erik.bulow@rccvast.se>
2016-05-11 12:44:43 +0200

    code corrections

--- data_raw/classcodes_hip_adverse_events_icd10.R
+++ data_raw/classcodes_hip_adverse_events_icd10.R
@@ -1,8 +1,8 @@
 hip_adverse_events_icd10 <- tibble::frame_data(
 
     ~group,              ~regex,                                                  ~condition,
-    "vascular deseases", "I|(J(81|1[358]))|R33|(K((2([56]|7[0-6]))|923))",             "hdia",
-    "luxation",          "(T((8((1[034])|4[05]))|933))|L899|S730|M24[34]|I(803|269)", NA
+    "vascular deseases", "^(I|(J(81|1[358]))|R33|(K((2([56]|7[0-6]))|923)))",             "hdia",
+    "luxation",          "^((T((8((1[034])|4[05]))|933))|L899|S730|M24[34]|I(803|269))", NA
 
     ) %>%
   icdswe::as.classcodes()
