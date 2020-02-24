26f28f4187a7dcca839f5f656c78c440066db112
Erik Bülow <eriklgb@gmail.com>
2020-02-24 13:17:23 +0100

    stringsAsFactors = FALSE

--- data-raw/example_cardata.R
+++ data-raw/example_cardata.R
@@ -5,7 +5,8 @@ ex_cars <-
   data.frame(
     id        = sample(randomNames::randomNames(N),   N, replace = TRUE),
     code      = rownames(mtcars)[sample(nrow(mtcars), N, replace = TRUE)],
-    code_date = Sys.Date() - sample(N)
+    code_date = Sys.Date() - sample(N),
+    stringsAsFactors = FALSE
   )
 
 usethis::use_data(ex_cars, overwrite = TRUE)
