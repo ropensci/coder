2bac2478d7b5760804ca13746995005211d890d7
Erik Bülow <eriklgb@gmail.com>
2020-04-14 08:08:23 +0200

    codebooks functions

--- R/codebook.R
+++ R/codebook.R
@@ -1,3 +1,13 @@
+# Helper to export to file
+fileif <- function(x, file = NULL) {
+  if (!is.null(file)) {
+    writexl::write_xlsx(x, file)
+    invisible(x)
+  } else {
+    x
+  }
+}
+
 
 #' Make codebook for classcodes object
 #' @inheritParams summary.classcodes
@@ -6,6 +16,7 @@
 #'
 #' @return List of data frames describing relationship
 #' between groups and individual codes
+#' @seealso codebooks
 #' @export
 #'
 #' @examples
@@ -23,7 +34,7 @@ codebook <- function(object, coding, ..., file = NULL) {
 
   # All codes listed
   kv <- decoder_data(coding)
-  all_codes <- as.keyvalue(object, coding, ...)
+  all_codes <- suppressMessages(as.keyvalue(object, coding, ...))
   all_codes <- merge(kv, all_codes, by = "key")
   names(all_codes) <- c("code", "description", "group")
 
@@ -46,12 +57,29 @@ codebook <- function(object, coding, ..., file = NULL) {
     )
 
   cb <- list(readme = readme, summary = s, all_codes = all_codes)
+  fileif(cb, file)
+}
 
-  # Export to Excel file if file specified
-  if (!is.null(file)) {
-    writexl::write_xlsx(cb, file)
-    invisible(cb)
-  } else {
-    cb
-  }
+#' Combine codebooks for classcodes objects
+#'
+#' @param ... named output from \code{\link{codebook}}
+#' @param file name of Excel file for data export
+#'
+#' @return Concatenated list with output from \code{\link{codebook}}.
+#' Only one 'README' object is kept however and renamed as such.
+#' @export
+#' @seealso codebook
+#'
+#' @examples
+#' c1 <- codebook(elixhauser, "icd10")
+#' c2 <- codebook(elixhauser, "icd9cmd",
+#'   cc_args = list(regex = "regex_icd9cm_enhanced")
+#'   )
+#' codebooks(elix_icd10 = c1, elix_icd9cm = c2)
+codebooks <- function(..., file = NULL) {
+  cbs <- unlist(list(...), FALSE)
+  rdm <- grep(".readme", names(cbs))
+  cbs <- cbs[-rdm[-1]] # Rm all readmes except the first
+  names(cbs) <- gsub("(.*)(\\.readme)", "README", names(cbs))
+  fileif(cbs, file)
 }
