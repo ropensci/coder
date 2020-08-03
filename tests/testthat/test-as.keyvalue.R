d3be79cd4418af424bc78fa7c1a1b8c27726d0af
Erik Bülow <eriklgb@gmail.com>
2020-08-03 20:13:11 +0200

    Fix #108.

---
+++ tests/testthat/test-as.keyvalue.R
@@ -0,0 +1,13 @@
+
+a <- decoder::as.keyvalue(elixhauser, "icd10se")
+
+
+test_that("as.keyvalue", {
+  expect_message(
+    decoder::as.keyvalue(elixhauser, "icd10se"),
+    "Classification based on: regex_icd10"
+  )
+  expect_s3_class(a, "keyvalue")
+  expect_gte(nrow(a), 1500)
+})
+
